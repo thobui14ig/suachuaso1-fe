0bb78d96caf31eaf01c856888a1825b146d63243
thobui14ig <buithanhtho14ig@gmail.com>
2023-04-05 22:22:24 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
-import { ChatComponent } from './chat/chat.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { WelcomeModule } from './pages/welcome/welcome.module';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'chat', component: ChatComponent },
+  { path: 'welcome', component: WelcomeModule },
+  { path: 'chat', component: ChatComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
-import { ChatComponent } from './chat/chat.component';
-import { UserOnlineComponent } from './chat/components/user-online/user-online.component';
-import { ChatBodyComponent } from './chat/components/chat-body/chat-body.component';
 import { ApiService } from './api/api.service';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
@@ -22,17 +14,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ChatBodyComponent } from './pages/chat/components/chat-body/chat-body.component';
+import { UserOnlineComponent } from './pages/chat/components/user-online/user-online.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
     ChatComponent,
     ChatBodyComponent,
     UserOnlineComponent
